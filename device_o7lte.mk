04d96a6381c7d5c3d561df462628b365de37be3c
FireLord1 <firelord.xda@gmail.com>
2016-08-04 20:25:37 +0530

    o7lte: add 2gb ram infos

--- device_o7lte.mk
+++ device_o7lte.mk
@@ -5,6 +5,9 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/o7lte/o7lte-vendor.mk)
 
+# Dalvik VM specific for devices with 2048 MB of RAM
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 #DEVICE_PACKAGE_OVERLAYS += device/samsung/o7lte/overlay
 
 
